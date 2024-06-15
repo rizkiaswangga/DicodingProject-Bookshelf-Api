62bfff8d58bc6d6a56cdaf509a3936c9598e8eb8
rizkiaswangga <59631372+rizkiaswangga@users.noreply.github.com>
2024-06-16 04:07:35 +0700

    Can successfully add a new book

---
+++ src/handler.js
@@ -0,0 +1,39 @@
+const books = require('./books');
+const { nanoid } = require('nanoid');
+
+const addBookHandler = (request, h) => {
+    const { name, year, author, summary, publisher, pageCount, readPage, reading } = request.payload;
+    const id = nanoid(16);
+    const finished = pageCount === readPage;
+    const insertedAt = new Date().toString();
+    const updatedAt = insertedAt;
+
+    const newBook = {
+        id, name, year, author, summary, publisher, pageCount, readPage, finished, reading, insertedAt, updatedAt
+    }
+
+    books.push(newBook);
+
+    const isSuccess = books.filter((book) => book.id === id).length > 0;
+
+    if (isSuccess) {
+        const response = h.response({
+            status: 'success',
+            message: 'Buku berhasil ditambahkan',
+            data: {
+                bookID: id,
+            },
+        });
+        response.code(201);
+        return response;
+    }
+
+    const response = h.response({
+        status: 'fail',
+        message: 'Buku gagal ditambahkan',
+    });
+    response.code(500);
+    return response;
+};
+
+module.exports = { addBookHandler };
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,12 @@
+const { addBookHandler } = require('./handler')
 
 const routes = [
-    
+    // API menyimpan buku :
+    {
+        method: 'POST',
+        path: '/books',
+        handler: addBookHandler,
+    },
 ]
 
 module.exports = routes;
