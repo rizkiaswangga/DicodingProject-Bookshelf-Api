f05f17d2e9ef488aa3c47a10f344598d17baac0f
rizkiaswangga <59631372+rizkiaswangga@users.noreply.github.com>
2024-06-18 17:00:30 +0700

    all add handlers are successfully created

--- src/handler.js
+++ src/handler.js
@@ -12,6 +12,32 @@ const addBookHandler = (request, h) => {
         id, name, year, author, summary, publisher, pageCount, readPage, finished, reading, insertedAt, updatedAt
     }
 
+    const correctRequirements = ( ( newBook.name !== undefined ) && newBook.readPage <= newBook.pageCount);
+    console.log(correctRequirements);
+
+    if (correctRequirements === false) {
+
+        if ( newBook.name == undefined ){
+            const response = h.response({
+                status: 'fail',
+                message: 'Gagal menambahkan buku. Mohon isi nama buku',
+            });
+
+            response.code(400);
+            return response;
+        }
+
+        if ( newBook.readPage > newBook.pageCount ){
+            const response = h.response({
+                status: 'fail',
+                message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
+            });
+
+            response.code(400);
+            return response;
+        }
+    }
+
     books.push(newBook);
 
     const isSuccess = books.filter((book) => book.id === id).length > 0;
